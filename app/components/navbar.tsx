1ceab9dce656d1cd4a3e87d4f1d67cf6db79c827
Alireza Akbari <alirezajj118@gmail.com>
2025-08-31 17:23:29 +0300

    third commit

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState } from "react";
 import Image from "next/image";
+import Link from 'next/link';
+
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false); // To control the menu state for mobile view
@@ -8,9 +10,9 @@ const Navbar = () => {
   return (
     <div className="bg-white shadow-md fixed top-0 left-0 right-0 z-50">
       <div className=" flex justify-center items-center h-full">
-        <a href="/">
+        <Link href="/">
           <Image src="/images/logo.png" alt="logo" width={150} height={150} />
-        </a>
+        </Link>
       </div>
     </div>
   );
